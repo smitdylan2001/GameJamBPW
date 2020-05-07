3709c69bb71cc5917a6d4b9947e27b83c42c0e65
Dylan Smit <dylan.smit@hotmail.com>
2020-05-07 14:01:40 +0200

    Added spawn system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public int Points;
+    float mousePos;
+    public int poppetje;
+
+    public GameObject explode;
+    public GameObject magnetic;
+    public GameObject floaty;
+    public GameObject build;
+    GameObject currentSelected;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +23,31 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePos = Input.mousePosition.x;
+            SpawnIt(poppetje);
+        }
+    }
+
+    void SpawnIt(int spawnThis)
+    {
+        if (spawnThis == 1)
+        {
+            currentSelected = explode;
+        }
+        if (spawnThis == 2)
+        {
+            currentSelected = magnetic;
+        }
+        if (spawnThis == 3)
+        {
+            currentSelected = floaty;
+        }
+        if (spawnThis == 4)
+        {
+            currentSelected = build;
+        }
+        Instantiate(currentSelected, new Vector2(mousePos, 15), Quaternion.identity);
     }
 }
