911301620b3848436ff8e4ec5df849264f8e46ef
knuffelturk <aran1997.ay@gmail.com>
2020-05-07 13:56:52 +0200

    Magnetisch poppetje

---
+++ Assets/Scripts/Poppetjes/Magnetism.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnetism : MonoBehaviour
+{
+    public float magnetismRadius, magnetismStrength;
+    Vector2 targetLocation;
+    bool targetFound = false;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, magnetismRadius);
+        if (colliders.Length > 0)
+        {
+            targetLocation = colliders[0].transform.position;
+            targetFound = true;
+        }
+
+        if (targetFound = true && rb != null)
+        {
+            rb.AddForce(new Vector2(targetLocation.x - transform.position.x, targetLocation.y - transform.position.y) * magnetismStrength / Vector2.Distance(targetLocation, transform.position));
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        //Destroy(rb);
+    }
+}
